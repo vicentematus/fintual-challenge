3c0c8be7ebfc0b49b3c69080df5b49053e135ce0
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2025-10-30 04:30:33 -0300

    feat(schema): add schema imports in init to use it in other modules

---
+++ src/schemas/__init__.py
@@ -0,0 +1,15 @@
+from .allocation import Allocation
+from .holding import Holding
+from .rebalance import RebalanceResult, Rebalance, RebalanceAction
+from .stocks import Stocks, Stock, StockSymbol
+
+__all__ = [
+    "Allocation",
+    "Holding",
+    "RebalanceResult",
+    "Rebalance",
+    "RebalanceAction",
+    "Stocks",
+    "Stock",
+    "StockSymbol",
+]
